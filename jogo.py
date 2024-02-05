c22651806dd18016378a7f95725f5649109686a7
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-05 10:01:05 -0300

    feat: hero atack and special atacks

--- jogo.py
+++ jogo.py
@@ -42,6 +42,11 @@ class Heroi(Personagem):
     def exibir_detalhes(self):
         return f"\n{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
     
+    def ataque_especial(self, alvo):
+        dano = self.get_nivel() * 5
+        alvo.receber_ataque(dano)
+        print(f"\n{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano")
+    
 class Inimigo(Personagem):
     def __init__(self, nome, vida, nivel, tipo) -> None:
         super().__init__(nome, vida, nivel)
@@ -73,13 +78,19 @@ class Jogo:
 
             if escolha == 1:
                 self.heroi.atacar(self.inimigo)
+            elif escolha == 2:
+                self.heroi.ataque_especial(self.inimigo)
             else:
                 print("Escolha inválida, escolha novamente")
+            
+            if self.inimigo.get_vida() > 0:
+                '''Inimigo ataca o heroi'''
+                self.inimigo.atacar(self.heroi)
 
         if self.heroi.get_vida() > 0:            
-            print(f"\nParabéns você venceu a batalha")
+            print(f"\nParabéns você venceu a batalha\n")
         else:
-            print(f"\nInfelizemente você perdeu a batalha")
+            print(f"\nInfelizmente você perdeu a batalha\n")
 
 jogo = Jogo()
 jogo.iniciar_batalha()
